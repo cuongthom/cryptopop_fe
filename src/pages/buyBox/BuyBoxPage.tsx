48becdcdc589e37db01e3e352c22ae27a880dd2d
cuongthom <cuong110203@gmail.com>
2024-09-19 01:08:21 +0700

    fix buy box

--- src/pages/buyBox/BuyBoxPage.tsx
+++ src/pages/buyBox/BuyBoxPage.tsx
@@ -77,31 +77,12 @@ const BuyBoxPage = () => {
     }
   };
   const { data: signer } = useSigner();
-  const getCuong = async () => {
-    const mes = "xin chao cuong";
-    const body = {
-      address: "0x23dFb47f22832aD8e9701F6C9743D2986c5D1CdA",
-      merNumber: 1,
-      type: "0x23dFb47f22832aD8e9701F6C9743D2986c5D1CdA",
-      signature: signer,
-      dataThatWasSigned: mes,
-    };
-    try {
-      await popApi.getSigner(body);
-    } catch (err) {
-      console.log(err);
-    }
-  };
+
   return (
     <section className="px-20 text-xl pb-4">
       <h2 className="py-5 pb-12 font-bold text-base">Cryptopops | Buy box</h2>
       <div className="flex ">
-        <div className="w-100 w-1/2">
-          <video height="auto" className="m-0 " autoPlay={true} loop={true}>
-            <source src="/image/videoPop.90fcae92.mp4" type="video/mp4" />
-            Your browser does not support the video tag.
-          </video>
-        </div>
+
         <div className="w-1/2 px-14">
           <p className="text-black font-bold text-4xl">CryptoPop</p>
           <p className="text-black py-8 text-xl">
